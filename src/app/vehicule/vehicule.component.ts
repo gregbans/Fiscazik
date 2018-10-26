bebba13a20b101c551bef814f105243323bc1c9a
bans <gregory.bannier@gmail.com>
2018-10-26 14:50:54 +0200

    v5
    suppression des components en trop pour vehicule.
    works!

--- src/app/vehicule/vehicule.component.ts
+++ src/app/vehicule/vehicule.component.ts
@@ -3,6 +3,8 @@ import { Vehicule } from '../models/vehicule.model';
 import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
 import { VehiculeService } from '../services/vehicule.service';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-vehicule',
@@ -13,10 +15,15 @@ export class VehiculeComponent implements OnInit, OnDestroy {
 
 vehicules : Vehicule[];
 vehiculesSubscription: Subscription;
+vehiculeForm: FormGroup;
 
-  constructor(private vehiculeService: VehiculeService, private router: Router) { }
+  constructor(private vehiculeService: VehiculeService,
+              private router: Router,
+              private formBuilder: FormBuilder,
+              ) { }
 
   ngOnInit() {
+    this.initForm();
     this.vehiculesSubscription = this.vehiculeService.vehiculeSubject.subscribe(
       (vehicules:Vehicule[])=>{
         this.vehicules = vehicules;
@@ -25,21 +32,39 @@ vehiculesSubscription: Subscription;
     this.vehiculeService.emitVehicules();
   }
 
-  onNewVehicule(){
-    this.router.navigate(['/vehicule', 'new']); // a verifier si faut pas faire sauter le route 'new'
-  }
+  initForm(){
+    this.vehiculeForm = this.formBuilder.group(
+    {
+    marque: ['', Validators.required],
+    modele: ['', Validators.required],
+    puissance: [+'', Validators.required],
+    }
+    )
+    }
+
+  // onNewVehicule(){
+  //   this.router.navigate(['/vehicule', 'new']); // a verifier si faut pas faire sauter le route 'new'
+  // }
 
   onDeleteVehicule(vehicule: Vehicule){
     this.vehiculeService.removeVehicule(vehicule);
   }
 
-  onViewVehicule(id: number){
-    this.router.navigate(['/vehicule', 'view', id]); // a verifier si faut pas faire sauter le route 'view'
-  }
+  // onViewVehicule(id: number){
+  //   this.router.navigate(['/vehicule', 'view', id]); // a verifier si faut pas faire sauter le route 'view'
+  // }
 
   ngOnDestroy(){
     this.vehiculesSubscription.unsubscribe();
   }
 
+  onSaveVehicule(){
+    const marque = this.vehiculeForm.get('marque').value;
+    const modele = this.vehiculeForm.get('modele').value;
+    const puissance = this.vehiculeForm.get('puissance').value;
+    const newVehicule = new Vehicule(marque, modele, puissance );
+    this.vehiculeService.createNewVehicule(newVehicule);
+    this.router.navigate(['/vehicule']);
+    }
 
 }
