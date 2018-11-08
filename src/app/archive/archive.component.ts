d60a0b2bcf717e347e63618141add05c14628c75
Nicolas LAURIO <nicolas.laurio@southside-interactive.com>
2018-11-08 19:14:58 +0100

    wip

--- src/app/archive/archive.component.ts
+++ src/app/archive/archive.component.ts
@@ -11,16 +11,17 @@ export class ArchiveComponent implements OnInit {
   diagnosticEmail= "";
 
   constructor(private ds: DiagnosticsService) {
-    this.ds.getDiagnostic("gregory.bannier@gmail.com").subscribe(
-      (res: any) => {
-      
-        console.log("archive.component.ts res", res)
-        this.diagnosticEmail= (res.email_diagnostic);
-      }, err =>(console.log("archive.component.ts erreur", err))
-  )
+ 
   }
 
   ngOnInit() {
+    this.ds.getDiagnostic("gregory.bannier@gmail.com").subscribe(
+      (res: any) => {
+      
+        console.log("archive.component.ts res d", res)
+        this.diagnosticEmail= res.email_diagnostic;
+      }, err =>(console.log("archive.component.ts erreur", err))
+  )
   }
 
 }
